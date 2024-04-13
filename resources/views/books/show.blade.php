fb30d77fd4726f34f321fb1d64b23245a7423680
pyittainghtaung <aungsoemin.personal@gmail.com>
2024-04-13 16:08:05 +0630

    Scoped Resource Controller - Adding a review
    In this chapter we practice how to make scoped resource controller.
    create and store for review resource controller.

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -14,11 +14,16 @@
                     <x-star-rating :rating="$book->reviews_avg_rating" />
                 </div>
                 <span class="book-review-count text-sm text-gray-500">
-                    {{ $book->reviews_count }} {{ Str::plural('review', 5) }}
+                    {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count) }}
                 </span>
             </div>
         </div>
     </div>
+
+    <div class="mb-4">
+        <a href="{{ route('books.reviews.create',$book) }}" class="reset-link">Add a Review</a>
+    </div>
+
 
     <div>
         <h2 class="mb-4 text-xl font-semibold">Reviews</h2>
